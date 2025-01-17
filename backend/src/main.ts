3c66df8d5248bdfd713de965e1717b1c06ad3ae3
Carlos Alexandre <alexfilho062005@gmail.com>
2025-01-16 21:36:10 -0300

    :wrech: config cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3000'],
+    },
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
